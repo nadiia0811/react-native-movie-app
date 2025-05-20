db5babae8b9cfdefd14fb307ed4b35ac41b9f2c1
Nadya <nb1985@ukr.net>
2025-05-20 13:23:34 +0300

    feat: implement generic useFetch hook for reusable data fetching

---
+++ services/useFetch.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+interface UseFetchProps<T> {
+  fetchFunction: () => Promise<T>
+}
+
+const useFetch = <T>({ fetchFunction }: UseFetchProps<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+        setLoading(true);
+        setError(null);
+
+        const result = await fetchFunction();
+
+        setData(result);
+        
+    } catch (error) {
+        //@ts-ignore
+        setError(error instanceof Error ? error : new Error("An error occured"));
+    } finally {
+        setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, reset, fetchData };
+};
+
+export default useFetch;
