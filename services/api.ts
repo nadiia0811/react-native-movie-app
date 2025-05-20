81304561406ba7ff96b9b459da0df7488101b4ef
Nadya <nb1985@ukr.net>
2025-05-20 10:04:50 +0300

    feat: fetch movies from TMDB API

---
+++ services/api.ts
@@ -0,0 +1,31 @@
+export const TMDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_TMDB_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_TMDB_API_KEY}`,
+  },
+};
+
+interface FetchMoviesProps {
+  searchQuery?: string;
+}
+
+export const fetchMovies = async ({ searchQuery }: FetchMoviesProps) => {
+  const endPoint = searchQuery ? 
+    `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(searchQuery)}` 
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+  const response = await fetch(
+    `${endPoint}&query=${searchQuery}`, {
+      method: "GET",
+      headers: TMDB_CONFIG.headers,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch movie: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data.results;
+};
